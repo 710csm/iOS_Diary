c4739812621ab7f8a12225589e2a4d0fc890cff0
Choi SeungMyeong <710csm@naver.com>
2020-09-07 15:22:33 +0900

    Initial Commit

---
+++ TimeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTable
+//
+//  Created by 최승명 on 2020/09/07.
+//  Copyright © 2020 최승명. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
